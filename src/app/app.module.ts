9d73d7094c5551097986724f6e741260ef7cc5de
javeed <karishmavahida@gmail.com>
2023-02-20 14:32:25 +0530

    Reusable component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
-import { ChildComponent } from './child/child.component'
+import { ChildComponent } from './child/child.component';
+import { UserDetailsComponent } from './user-details/user-details.component'
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent
+    ChildComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
