671d723587c408104141e4601573ec88a6eaf31f
javeed <karishmavahida@gmail.com>
2023-02-20 13:00:52 +0530

    Todolist is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularPractice2';
+  list:any[] =[]
+
+  addTask(item:string){
+    this.list.push({id:this.list.length,name: item})
+    console.log(this.list);
+  }
+
+  remove(id:number){
+    this.list = this.list.filter(item => item.id !== id)
+    console.log(this.list);
+    
+  }
 }
